e14c3493c33fd37486925d3de3049c2a140b9007
Vasyl Melnychuk <vasyl_melnychuk@epam.com>
2014-04-21 22:46:08 +0300

    add udacity mongodb course

---
+++ udacity/mongodb/python/parseCSV.py
@@ -0,0 +1,26 @@
+import os
+
+DATADIR = ""
+DATAFILE = "beatles-diskography.csv"
+
+def parse_file(datafile):
+    data = []
+    with open(datafile, "rb") as f:
+        header = f.readline().split(",")
+        counter = 0
+        for line in f:
+            if counter == 10:
+                break
+            fields = line.split(",")
+            entry = {}
+            for i, value in enumerate(fields):
+                entry[header[i].strip()] = value.strip()
+            data.append(entry)
+            counter += 1
+    return data
+
+def test():
+    datafile = os.path.join(DATADIR, DATAFILE)
+    d = parse_file(datafile)
+    print d[1]
+test()
